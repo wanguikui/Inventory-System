5f5334a77b2586ce7a72581ad33e177eb1c61111
wanguikui <wangui.munyua@gmail.com>
2020-05-11 13:47:01 -0700

    First Commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template,request, redirect, url_for, flash
+app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run()
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/inventories')
+def inventories():
+    return render_template('inventories.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
+@app.route('/data_visualization')
+def data_visualization():
+    return render_template('charts.html')
